5e346fc943f0bb6dcf2227a1fb5379823ab0d903
Rick Cha <rick.cha@outlook.com>
2020-02-29 12:55:51 -0800

    error handling

--- src/authenticate_user.js
+++ src/authenticate_user.js
@@ -1,6 +1,21 @@
-export const handler = () => ({
-  statusCode: 200,
-  body: JSON.stringify({
-    message: 'authenticate_user',
-  }),
-});
+export const handler = evt => {
+  try {
+    console.log('hello world');
+    return {
+      statusCode: 200,
+      headers: {
+        'Access-Control-Allow-Origin': '*',
+      },
+      body: JSON.stringify({ message: 'Hello, authenticated user' }),
+    };
+  } catch (err) {
+    console.log(err);
+    return {
+      statusCode: 500,
+      headers: {
+        'Access-Control-Allow-Origin': '*',
+      },
+      body: JSON.stringify(err),
+    };
+  }
+};
