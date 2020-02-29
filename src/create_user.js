2c2d1b4e0de9307e626909c8075e43608200274b
Rick Cha <rick.cha@outlook.com>
2020-02-29 12:11:43 -0800

    some evn configurations

---
+++ src/create_user.js
@@ -0,0 +1,6 @@
+export const handler = () => ({
+  statusCode: 200,
+  body: JSON.stringify({
+    message: 'create_user',
+  }),
+});
---
+++ websocket/disconnect.js
@@ -0,0 +1,6 @@
+export const handler = () => ({
+  statusCode: 200,
+  body: JSON.stringify({
+    age: 12,
+  }),
+});
