75fe332650aceba7b2a4ce0e146ce09fcaf97c02
Rick Cha <rick.cha@outlook.com>
2020-04-17 14:07:12 -0700

    clean up before config-overrides

---
+++ src/routes/rooms/find.js
@@ -0,0 +1,31 @@
+import { verifyHeader } from '../../helpers/token';
+import Room from '../../models/rooms';
+import User from '../../models/users';
+
+export const handler = async evt => {
+  try {
+    const { headers, body } = evt;
+    const data = JSON.parse(body);
+
+    await verifyHeader(headers);
+
+    const { roomId } = data;
+
+    console.log(roomId);
+    const room = await Room.find({ id: roomId });
+
+    const { userIds } = room;
+    room.users = await User.listIn('id', userIds);
+
+    return {
+      statusCode: 200,
+      body: JSON.stringify(room),
+    };
+  } catch (err) {
+    console.log(err);
+    return {
+      statusCode: 500,
+      body: JSON.stringify(err, ['name', 'message']),
+    };
+  }
+};
